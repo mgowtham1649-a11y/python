78372cd972a49307aec6a051e71620d4d3b7f898
mgowtham1649-a11y <mgowtham1649@gmail.com>
2026-07-21 20:47:07 +0530

    Add files via upload

---
+++ summean.py
@@ -0,0 +1,8 @@
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: "))
+
+sum=a+b+c
+mean=sum/3
+print("sum:",sum)
+print("mean:",mean)
